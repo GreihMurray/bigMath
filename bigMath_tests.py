07c30b35d6efbf31ae26c98a65a2cf2df1ed65ee
Greih Murray <murrayg0216@outlook.com>
2021-04-07 08:48:02 -0500

    added tests for reverseLsit method (All pass)

--- bigMath_tests.py
+++ bigMath_tests.py
@@ -8,3 +8,10 @@ class TestMath(unittest.TestCase):
         self.assertAlmostEqual(bigMath.convertToDecimal(str(1.25), str(3)), [1, 2, 5, 0])
         self.assertAlmostEqual(bigMath.convertToDecimal(str(3.254), str(5)), [3, 2, 5, 4, 0, 0])
         self.assertAlmostEqual(bigMath.convertToDecimal(str(5.6987), str(12)), [5, 6, 9, 8, 7, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_reverseList(self):
+        self.assertAlmostEqual(bigMath.reverseList([2, 5, 4, 3, 6]), [6, 3, 4, 5, 2])
+        self.assertAlmostEqual(bigMath.reverseList([7, 6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6, 7])
+        self.assertAlmostEqual(bigMath.reverseList([9, 5, 1, 3, 7]), [7, 3, 1, 5, 9])
+        self.assertAlmostEqual(bigMath.reverseList([9, 3, 1, 7]), [7, 1, 3, 9])
+        self.assertAlmostEqual(bigMath.reverseList([3, 6, 9, 8, 7, 4, 1, 2, 5]), [5, 2, 1, 4, 7, 8, 9, 6, 3])
