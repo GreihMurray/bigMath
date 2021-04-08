1b5c7a015e230b0eab8682ea2eaedf075c545955
Greih Murray <murrayg0216@outlook.com>
2021-04-08 06:50:30 -0500

    Added tests for convertToScientificNotation method (All Pass)

--- bigMath_tests.py
+++ bigMath_tests.py
@@ -15,3 +15,10 @@ class TestMath(unittest.TestCase):
         self.assertAlmostEqual(bigMath.reverseList([9, 5, 1, 3, 7]), [7, 3, 1, 5, 9])
         self.assertAlmostEqual(bigMath.reverseList([9, 3, 1, 7]), [7, 1, 3, 9])
         self.assertAlmostEqual(bigMath.reverseList([3, 6, 9, 8, 7, 4, 1, 2, 5]), [5, 2, 1, 4, 7, 8, 9, 6, 3])
+
+    def test_convertToScientificNotation(self):
+        self.assertAlmostEqual(bigMath.convertToScientifcNotation([2, 9, 8, 6, 0, 0, 0]), ("2.986", 6))
+        self.assertAlmostEqual(bigMath.convertToScientifcNotation([4, 3, 6, 0, 2, 0, 0]), ("4.3602", 6))
+        self.assertAlmostEqual(bigMath.convertToScientifcNotation([5, 0, 2, 3, 0, 0]), ("5.023", 5))
+        self.assertAlmostEqual(bigMath.convertToScientifcNotation([9, 4, 0, 2]), ("9.402", 3))
+        self.assertAlmostEqual(bigMath.convertToScientifcNotation([8, 9, 4, 3, 5, 2, 5, 0, 0, 2, 0]), ("8.943525002", 10))
